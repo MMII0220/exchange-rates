c5b4e65f48badc02e9e180289fa154fa9bdc7ab8
MMII0220 <abdulloevmuhammadcon36@gmail.com>
2022-05-28 14:22:31 +0300

    finished html css

---
+++ js/bundle.js
@@ -0,0 +1,11 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+var __webpack_exports__ = {};
+/*!*****************!*\
+  !*** ./main.js ***!
+  \*****************/
+
+
+/******/ })()
+;
+//# sourceMappingURL=bundle.js.map
